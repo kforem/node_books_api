ee5c8ff27bc04eaa74836423387739de33503c8a
kwasniew <kwasniewski.mateusz@gmail.com>
2020-11-03 15:42:41 +0100

    routes

--- src/app.js
+++ src/app.js
@@ -1,51 +1,12 @@
 const express = require("express");
-const MongoClient = require('mongodb').MongoClient;
-
 const app = express();
-
-const url = 'mongodb://localhost:27017/booksapi';
-
-let booksPromise = MongoClient.connect(url).then(function (client) {
-    return client.db().collection("books");
-});
-
-// application scope, request scope
+const bookRoutes = require("./bookRoutes");
 
 app.use(express.json());
 app.get("/", function (req, res, next) {
     res.send("Hello World!");
 });
-app.post("/book", function (req, res, next) {
-    const {title, authors, isbn, description} = req.body;
-    booksPromise
-        .then(function (books) {
-            return books.updateOne(
-                {isbn: isbn},
-                {$set: {title, authors, isbn, description}},
-                {upsert: true}
-            );
-        })
-        .then(function () {
-            res.json({title, authors, isbn, description});
-        })
-        .catch(next);
-
-
-});
-app.get("/book/:isbn", function (req, res, next) {
-    const isbn = req.params.isbn;
-    booksPromise
-        .then(function (books) {
-            return books.findOne(
-                {isbn},
-                {projection: {_id: 0}}
-            );
-        })
-        .then(function (book) {
-            res.json(book);
-        })
-        .catch(next);
-});
+app.use("/", bookRoutes);
 
 app.use(function (req, res, next) {
     const err = new Error("Not Found");
