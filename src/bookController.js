84caef6b2c5c85570ff01853fa3d64864d7afe11
kwasniew <kwasniewski.mateusz@gmail.com>
2020-11-04 11:20:06 +0100

    controller test

--- src/bookController.js
+++ src/bookController.js
@@ -1,26 +1,35 @@
-const bookRepository = require("./bookRepository");
-const bookService = require("./bookService");
+// const bookRepository = require("./bookRepository");
+// const bookService = require("./bookService");
 
-module.exports = {
-    async createOrUpdate(req, res, next) {
-        // HTTP
-        const {title, authors, isbn, description} = req.body;
-        try {
-            // JS
-            await bookService.createOrUpdate({title, authors, isbn, description});
+// class BookController {
+//     BookController() {
+//         this.bookRepository = new BookRepository();
+//         this.bookService = new BookService();
+//     }
+// }
+
+module.exports = ({bookService, bookRepository}) => {
+    return {
+        async createOrUpdate(req, res, next) {
             // HTTP
-            res.redirect(`/book/${isbn}`);
-        } catch (e) {
-            next(e);
+            const {title, authors, isbn, description} = req.body;
+            try {
+                // JS
+                await bookService.createOrUpdate({title, authors, isbn, description});
+                // HTTP
+                res.redirect(`/book/${isbn}`);
+            } catch (e) {
+                next(e);
+            }
+        },
+        async details(req, res, next) {
+            try {
+                const isbn = req.params.isbn;
+                const book = await bookRepository.findOne(isbn);
+                res.json(book);
+            } catch(e) {
+                next(e);
+            }
         }
-    },
-    async details(req, res, next) {
-        try {
-            const isbn = req.params.isbn;
-            const book = await bookRepository.findOne(isbn);
-            res.json(book);
-        } catch(e) {
-            next(e);
-        }
-    }
+    };
 };
