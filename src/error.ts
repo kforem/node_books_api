8a705b601ab076dae16e7808c40d430ac61993dc
kwasniew <kwasniewski.mateusz@gmail.com>
2020-11-05 12:20:11 +0100

    done

--- src/error.ts
+++ src/error.ts
@@ -1,21 +1,25 @@
 import { Request, Response, NextFunction } from "express";
 
+export class StatusError extends Error {
+  constructor(readonly error: string | object, readonly status: number) {
+    super();
+  }
+}
+
 export function notFound(req: Request, res: Response, next: NextFunction) {
-  const err = new Error("Not Found");
-  // @ts-ignore
-  err.status = 404;
+  const err = new StatusError("Not Found", 404);
   next(err);
 }
 
 export function errorHandler(
-  err: any,
+  err: StatusError,
   req: Request,
   res: Response,
   next: NextFunction
 ) {
   console.error(err.stack);
   res.status(err.status || 500);
-  res.json({ message: err.message, error: isProduction() ? {} : err.stack });
+  res.json({ message: err.error, error: isProduction() ? {} : err.stack });
 }
 
 function isProduction() {
