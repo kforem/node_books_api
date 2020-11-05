be40b9356690b8fab523ed0afd154f62f02ad01c
kwasniew <kwasniewski.mateusz@gmail.com>
2020-11-05 10:33:48 +0100

    book routes

--- src/bookcatalog/bookRoutes.ts
+++ src/bookcatalog/bookRoutes.ts
@@ -1,16 +1,19 @@
+import { resources } from "./links";
+import { bookRepositoryFactory } from "./bookRepository";
+import { bookServiceFactory } from "./bookService";
+import { Router } from "express";
+import { bookControllerFactory } from "./bookController";
+import { validate } from "./validateBookMiddleware";
+
 module.exports = (db) => {
-  const { BOOK, BOOK_COLLECTION } = require("./links").resources;
-  const router = require("express").Router();
-  const { bookRepositoryFactory } = require("./bookRepository");
+  const { BOOK, BOOK_COLLECTION } = resources;
+  const router = Router();
   const bookRepository = bookRepositoryFactory(db);
-  const { bookServiceFactory } = require("./bookService");
   const bookService = bookServiceFactory(bookRepository);
-  const { bookControllerFactory } = require("./bookController");
   const { createOrUpdate, details, getList } = bookControllerFactory({
     bookService,
     bookRepository,
   });
-  const { validate } = require("./validateBookMiddleware");
 
   router.post(BOOK_COLLECTION, validate, createOrUpdate);
   router.get(BOOK_COLLECTION, getList);
